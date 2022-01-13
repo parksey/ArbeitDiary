f0c9d5ca580248ce139455b1baeeb943589b481b
psy <parkseyeon99@naver.com>
2022-01-13 18:14:56 +0900

    login

---
+++ ZB/fastlms/src/main/java/com/zerobase/fastlms/member/entity/Member.java
@@ -0,0 +1,34 @@
+package com.zerobase.fastlms.member.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.asm.Advice.Local;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class Member {
+	@Id
+	private String userId;
+	 
+	private String userName;
+	private String phone;
+	private String password;
+	private LocalDateTime regDt;
+	
+	private boolean emailAuthYn;
+	private String emailAuthKey;
+	private LocalDateTime emailAuthDt;
+	
+	private String resetPasswordKey;
+	private LocalDateTime resetPasswordDt;
+}
